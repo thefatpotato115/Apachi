0f2d2ccc924b9900dd0ca3a8091233d4ae126d79
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-05-12 14:12:29 +0000

    Update proxy.js

--- src/files/proxy.js
+++ src/files/proxy.js
@@ -5,7 +5,7 @@ p_e.innerHTML = `<body>
 
 <div style="color:green;background-color:black;width:100%;height:100%">
 <center>
-<h1 style="color:green"> Proxy servers </h1>
+<h1 style="color:green"> Apachi Proxy servers </h1> 
 <br>
 <br>
 <br>
@@ -22,23 +22,68 @@ Alloy proxy variations and VPNS (paid features) </p1>
 <br>
 <br>
 
-<button id="taco_1" style="color:green;background-color:black;border-color:green;width:100px;height:40px"> taco proxy </button>
+<button id="taco_1" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://hscowe.sse.codesandbox.io/"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '> taco proxy </button>
 
-<button id="taco_2" style="color:green;background-color:black;border-color:green;width:100px;height:40px"> taco proxy2 </button>
+<button id="taco_2" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://zke41r.sse.codesandbox.io/"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '> taco proxy2 </button>
 
-<button id="taco_3" style="color:green;background-color:black;border-color:green;width:100px;height:40px"> taco proxy 3</button>
+<button id="taco_3" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://m4qh56.sse.codesandbox.io/"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '> taco proxy 3</button>
 
-<button id="taco_4" style="color:green;background-color:black;border-color:green;width:100px;height:40px"> taco proxy 4</button>
+<button id="taco_4" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://ytxrqv.sse.codesandbox.io/"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '> taco proxy 4</button>
 
-<button id="alloy_1" style="color:green;background-color:black;border-color:green;width:100px;height:40px"> Alloy proxy </button>
+<button id="alloy_1" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://ym8c5.sse.codesandbox.io/"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '> Alloy proxy </button>
 
-<button id="alloy_2" style="color:green;background-color:black;border-color:green;width:100px;height:40px">Alloy Proxy 2</button>
+<button id="alloy_2" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://t3ck9.sse.codesandbox.io/"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '>Alloy Proxy 2</button>
 
-<button id="alloy_3" style="color:green;background-color:black;border-color:green;width:100px;height:40px">Alloy Proxy 3</button>
+<button id="alloy_3" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://3ghost-deploy-seven.vercel.app/projects/2048/index.html"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '>Alloy Proxy 3</button>
 
-<button id="alloy_4" style="color:green;background-color:black;border-color:green;width:100px;height:40px"> Alloy Proxy 4</button>
+<button id="alloy_4" style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://3ghost-deploy-seven.vercel.app/projects/2048/index.html"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '> Alloy Proxy 4</button>
 
-<button id="VPN_1" style="color:red;background-color:black;border-color:green;width:100px;height:40px"> Express VPN </button>
+<button id="VPN_1" style="color:red;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://3ghost-deploy-seven.vercel.app/projects/2048/index.html"; iframe.style=" width:65%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+   '> Express VPN </button>
 <br>
 <br>
 <br>
