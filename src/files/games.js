423a4cbfd1bd7c6fc20aa8517a2d77ae2bb3b37d
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-05-15 15:41:33 +0000

    Update games.js

--- src/files/games.js
+++ src/files/games.js
@@ -2,16 +2,21 @@ function Apachi_Games() {
   var e = document.createElement("div");
 
   e.innerHTML = `
-  <iframe
-  width="100%"
-  height="100%"
-  src=" https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/1ee20621-61bc-4ec8-a8ec-5e839c2e6edc%2Flearn-to-fly-3.xml"
-  frameborder="0"
-  allowfullscreen
-></iframe>;
+  <button style='background-color:rgb(111,111,111)' 
+onclick='var iframe = document.createElement("iframe"); 
+iframe.src=" https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/1ee20621-61bc-4ec8-a8ec-5e839c2e6edc%2Flearn-to-fly-3.xml"; 
+iframe.style=" width:100%; height:100%";
+document.body.appendChild(iframe); 
+var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);
+document.body.removeChild(d) ;};
+d.innerHTML="X";
+d.style="background-color:red";
+document.body.appendChild(d);
+'>Learn to fly 3</button>
 
   `;
-  var tab = window.open();
+  var tab = window.Games();
   tab.document.body.appendChild(e);
 }
 Apachi_Games();
