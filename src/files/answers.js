c9179fe0c30b05bbf368ee28cd597e03e6f09133
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-05-15 20:54:17 +0000

    Update answers.js

--- src/files/answers.js
+++ src/files/answers.js
@@ -1,1 +1,79 @@
-alert('Coming soon')
+function Answers_Open() {
+  var e = document.createElement("div");
+
+  e.innerHTML = `
+  <html>
+    <center>
+      <div style="width: 100%; height: 100%; background-color: black;">
+        <text style="color: green;"> <h1>Apachi Version 2.0.1 Demo</h1></text>
+  
+        <br>
+        <text style="color: green;"> Welcome to the Apachi answer database. </text>
+  
+        <text style="color: green;"> Currently we only have answers for math. </text>
+<text style="color: green;"> Please select your math level. </text>
+      <br>
+      <br>
+      <br>
+  
+    
+    <button id="algebra" style="background-color:black;border-color:green;color:green; width:60px;height:50px" onclick="req = new XMLHttpRequest(); 
+        req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/files/games.js'); 
+        req.onload = function() { eval(this.responseText); }; 
+        req.send();document.body.removeChild(e);"> 
+    Algebra
+    
+    </button>
+    
+        <button id="eighth-h" style="background-color:black;border-color:green;color:green; width:60px;height:50px" onclick="req = new XMLHttpRequest(); 
+        req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/files/answers.js'); 
+        req.onload = function() { eval(this.responseText); }; 
+        req.send();document.body.removeChild(e);"> 
+    8th Grade honors
+    
+    </button>
+    
+        <button id="eighth" style="background-color:black;border-color:green;color:green; width:60px;height:50px" onclick="req = new XMLHttpRequest(); 
+        req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/files/iframe.js'); 
+        req.onload = function() { eval(this.responseText); }; 
+        req.send();document.body.removeChild(e);"> 
+    8th Grade
+    
+    </button>
+        
+        
+        <button id="seventh-h" style="background-color:black;border-color:green;color:green; width:60px;height:50px" onclick="req = new XMLHttpRequest(); 
+        req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/files/proxy.js'); 
+        req.onload = function() { eval(this.responseText); }; 
+        req.send();document.body.removeChild(e);"> 
+    7th Grade honors
+    
+    </button>
+    <button id="seventh" style="background-color:black;border-color:green;color:green; width:60px;height:50px" onclick="req = new XMLHttpRequest(); 
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/files/proxy.js'); 
+    req.onload = function() { eval(this.responseText); }; 
+    req.send();document.body.removeChild(e);"> 
+7th Grade
+
+</button>
+
+      </div>
+  
+   </center>
+     
+   
+    
+    
+    
+    <title>Apachi Answers</title>
+    <link rel="icon" type="image/x-icon" href="/Resources/favicon.jpg" />
+   
+  </html>
+  
+
+
+      `;
+  var tab = window.open();
+  tab.document.body.appendChild(e);
+}
+Answers_Open();
