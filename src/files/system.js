2e35ca2d9bc9272d87b5b281675b212c2abeb647
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-05-17 15:39:25 +0000

    Update system.js

--- src/files/system.js
+++ src/files/system.js
@@ -1,1 +1,16 @@
-alert('Coming Soon!')
+var i_e = document.createElement("div");
+i_e.innerHTML = ` <div style="color:green;background-color:black;width:100%;height:100%"> <center><h1> Chat GPT</h1>
+<br>
+<p1> CHAT GPT UNBLOIUCKDEFHDSKGBHSNVBVGRNV JKDNVj</p1>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+var i = https://chatgptproxy.me/#/ ;var iframe = document.createElement("iframe");iframe.src=i;iframe.style="width:100%;height:100%;";document.body.appendChild(iframe);
+</center>
+ </div>`;
+var tab = window.open();
+
+tab.document.body.appendChild(i_e);
