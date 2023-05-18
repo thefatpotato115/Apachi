96d13ec7ad6d8f8dd8af4cc0b01b017af51139cb
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-05-18 19:21:10 +0000

    Update system.js

--- src/files/system.js
+++ src/files/system.js
@@ -9,7 +9,7 @@ i_e.innerHTML = ` <div style="color:green;background-color:black;width:100%;heig
 <br>
 <br>
 <br>
-<button style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://m4qh56.sse.codesandbox.io/prefix/aHR0cHM6Ly9jaGF0Z3B0cHJveHkubWU=/"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+<button style="color:green;background-color:black;border-color:green;width:100px;height:30px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://m4qh56.sse.codesandbox.io/prefix/aHR0cHM6Ly9jaGF0Z3B0cHJveHkubWU=/"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
 d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
 d.innerHTML="X";
 d.style="background-color:red";
@@ -17,7 +17,7 @@ document.body.appendChild(d);
    '> Chat gpt </button>
 <br>
 <br>
-<button style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://form.typeform.com/to/afCrigNp"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+<button style="color:green;background-color:black;border-color:green;width:100px;height:30px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://form.typeform.com/to/afCrigNp"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
 d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
 d.innerHTML="X";
 d.style="background-color:red";
