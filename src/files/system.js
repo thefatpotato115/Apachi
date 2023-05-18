236fcaf2c0fe2646329b05a5ec88c43c6c78c6ee
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-05-18 18:15:32 +0000

    Update system.js

--- src/files/system.js
+++ src/files/system.js
@@ -1,26 +1,22 @@
 var i_e = document.createElement("div");
-i_e.innerHTML = ` <div style="color:green;background-color:black;width:100%;height:100%"> <center><h1> Chat GPT</h1>
+i_e.innerHTML = ` <div style="color:green;background-color:black;width:100%;height:100%"> <center><h1> Apachi system</h1>
+<br>
+<p1> Apachi system is a everything else page for open-ai,game requests and bugfixes and more that we cant fit into a category</p1>
 <br>
 <br>
 <br>
 <br>
 <br>
 <br>
-<br>
-<button style='background-color:black;color:green;border-color:green;' 
-onclick='var iframe = document.createElement("iframe"); 
-iframe.src="https://9m1mf6.csb.app/"; 
-iframe.style=" width:100%; height:100%";
-document.body.appendChild(iframe); 
-var d = document.createElement("button");
-d.onclick= function () {document.body.removeChild(iframe);
-document.body.removeChild(d) ;};
+<button style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://m4qh56.sse.codesandbox.io/prefix/aHR0cHM6Ly9jaGF0Z3B0cHJveHkubWU=/"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
 d.innerHTML="X";
 d.style="background-color:red";
 document.body.appendChild(d);
-'>Open</button>
+   '> Chat gpt </button>
+   
+   <button data-tf-popup="afCrigNp" data-tf-opacity="100" data-tf-size="100" data-tf-iframe-props="title=My typeform" data-tf-auto-close="10000" data-tf-transitive-search-params data-tf-medium="snippet" style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#0FFF00;color:#000;font-size:20px;border-radius:25px;padding:0 33px;font-weight:bold;height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;">Request a game</button><script src="//embed.typeform.com/next/embed.js"></script> 
 </center>
-<title>System</title>
  </div>`;
 var tab = window.open();
 
