a93214b7a35a372f0928857536c636f297769d51
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-05-19 18:10:32 +0000

    Update system.js

--- src/files/system.js
+++ src/files/system.js
@@ -1,30 +1,34 @@
 var i_e = document.createElement("div");
-i_e.innerHTML = ` <div style="color:green;background-color:black;width:100%;height:100%"> <center>
-<h1> Apachi system</h1>
+i_e.innerHTML = ` <div style="color:green;background-color:black;width:100%;height:100%"> <center><h1> Apachi system</h1>
 <br>
-<p1> Apachi system is an 'everything else' page, and we currently only have chat GPT, and a game request form that we couldn't fit into another category.</p1>
-<br>
+<p1> Apachi system is a everything else page for open-ai,game requests and bugfixes and more that we cant fit into a category</p1>
 <br>
 <br>
 <br>
 <br>
 <br>
-<button style="color:green;background-color:black;border-color:green;width:100px;height:30px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://m4qh56.sse.codesandbox.io/prefix/aHR0cHM6Ly9jaGF0Z3B0cHJveHkubWU=/"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+<br>
+<button style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://m4qh56.sse.codesandbox.io/prefix/aHR0cHM6Ly9jaGF0Z3B0cHJveHkubWU=/"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
 d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
 d.innerHTML="X";
 d.style="background-color:red";
 document.body.appendChild(d);
    '> Chat gpt </button>
-<br>
-<br>
-<button style="color:green;background-color:black;border-color:green;width:100px;height:30px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://form.typeform.com/to/afCrigNp"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
-d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
-d.innerHTML="X";
-d.style="background-color:red";
-document.body.appendChild(d);
-   '> Game request </button>
-<div data-tf-widget="afCrigNp" data-tf-opacity="100" data-tf-iframe-props="title=My typeform" data-tf-medium="snippet" style="width:100%;height:500px;"></div><script src="//embed.typeform.com/next/embed.js"></script></center>
-<title>System</title>
+
+   <button style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://form.typeform.com/to/afCrigNp"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+   d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+   d.innerHTML="X";
+   d.style="background-color:red";
+   document.body.appendChild(d);
+      '> Chat gpt </button> 
+   
+   <button style="color:green;background-color:black;border-color:green;width:100px;height:40px" onclick='var iframe = document.createElement("iframe"); iframe.src="https://form.typeform.com/to/TmtkSH1E"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+   d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
+   d.innerHTML="X";
+   d.style="background-color:red";
+   document.body.appendChild(d);
+      '> Report a bug </button>
+</center>
  </div>`;
 var tab = window.open();
 
