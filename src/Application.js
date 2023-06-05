8f66074248ea817c882d4dae145aae63071c0cf5
keller coder <102377044+daschoolcoder@users.noreply.github.com>
2023-06-05 13:55:16 +0000

    Update Application.js

--- src/Application.js
+++ src/Application.js
@@ -46,7 +46,7 @@ function Apachi_Open() {
       
       <button id="proxy" style="background-color:black;border-color:green;color:green; width:60px;height:50px" onclick="req = new XMLHttpRequest(); 
       req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/files/proxy.js');
-      req.onload = function() { eval(this.responseText); }; 
+      req.onload = function() { window.close();eval(this.responseText); }; 
       req.send();"> 
   Proxy
   
