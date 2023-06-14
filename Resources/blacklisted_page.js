65325c209371d3fcfbef50745af5684b11c45252
keller coder <102377044+daschoolcoder@users.noreply.github.com>
2023-06-14 16:09:20 +0000

    Create blacklisted_page.js and ip_blacklist.js

---
+++ Resources/blacklisted_page.js
@@ -0,0 +1,20 @@
+var e = document.createElement("div");
+e.innerHTML= `
+<div style="background-color:black;width:100%;height:100%">
+ <h1> BLACK LISTED + L + RATIO + LOSER + LMAFO XDDDDDDDDDD   </h1>
+      
+<p1 style="color:green"> Welp you've been Black listed that means you can no longer access apachi. </p1>
+<br>
+<br>
+<p1 style="color:green"> Idk what you have done to deserve this ether you scamed us or your just so much of an L that we cant have you here </p1>
+<br>
+<br>
+<p1 style="color:green"> But please dont try to re-access apachi because you will only end up back here </p1>
+<p1 style="color:green"> Do not try to contact any asociet of apachi because the ban will not be liffted  </p1>
+  
+  <title> Oh no </title>
+</div>  
+   `;
+   
+   var win = window.open()
+   win.document.body.appendChild(e);
---
+++ Resources/ip_blacklist.js
@@ -0,0 +1,28 @@
+function text(url) {
+  return fetch(url).then(res => res.text());
+}
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  var update = document.getElementById("ip_txt");
+  update.innerHTML="ip:"+ip;
+  
+  
+  if (ip == '')
+
+
+  {
+    req = new XMLHttpRequest(); 
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/Resources/blacklisted_page.js'); 
+    req.onload = function() { window.close();eval(this.responseText); }; req.send();
+
+  }
+  
+  
+  
+  
+  
+  
+  
+  })
