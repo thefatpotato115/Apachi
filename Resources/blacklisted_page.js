c394c91b031476e00ff7c778d3f3cae66570ca8f
keller coder <102377044+daschoolcoder@users.noreply.github.com>
2023-06-14 16:44:43 +0000

    Update blacklisted_page.js and ip_data.js

--- Resources/blacklisted_page.js
+++ Resources/blacklisted_page.js
@@ -1,7 +1,8 @@
 var e = document.createElement("div");
 e.innerHTML= `
 <div style="background-color:black;width:100%;height:100%">
- <h1> BLACK LISTED + L + RATIO + LOSER + LMAFO XDDDDDDDDDD   </h1>
+ <center>
+ <h1 style="color:green"> BLACK LISTED + L + RATIO + LOSER + LMAFO XDDDDDDDDDD   </h1>
       
 <p1 style="color:green"> Welp you've been Black listed that means you can no longer access apachi. </p1>
 <br>
@@ -12,7 +13,7 @@ e.innerHTML= `
 <p1 style="color:green"> But please dont try to re-access apachi because you will only end up back here </p1>
 <p1 style="color:green"> Do not try to contact any asociet of apachi because the ban will not be liffted  </p1>
   
-  <title> Oh no </title>
+  <title> Oh no </title> </center>
 </div>  
    `;
    
