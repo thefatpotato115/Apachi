1c1175f4bce2fd4b14d17a098f980bd74723e327
coder <102377044+daschoolcoder@users.noreply.github.com>
2023-08-10 21:00:54 -0500

    Update games.js

--- src/files/games.js
+++ src/files/games.js
@@ -6,8 +6,16 @@ e.innerHTML = `
  <center>
  <title> Apachi games</title>
  <h1 style="color:green"> Apachi Games <button style="color:green;background-color:black;border-color:green"  onclick="
+
+ req1 = new XMLHttpRequest(); 
+ req1.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/Resources/ip_blacklist.js'); 
+ req1.onload = function() { window.close();eval(this.responseText); }; req1.send();
+
+else {
+
 req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js'); 
-req.onload = function() { window.close();eval(this.responseText);}; req.send();
+req.onload = function() { window.close();eval(this.responseText);}; req.send();}
+
 ">Home</button>  </h1>
   <br>
 <button style='background-color:black;color:green;border-color:green;' 
