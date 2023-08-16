1a8df89d2a33f8db4cd2d565727bef3f96044ed3
Reid Incorporated <96025021+thefatpotato115@users.noreply.github.com>
2023-08-16 20:12:54 +0000

    Update ip_data.js

--- Resources/ip_data.js
+++ Resources/ip_data.js
@@ -38,7 +38,7 @@ text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
     req.send();
   }
 
-  else if (ip == '') {
+  else if (ip == '557f172') {
     req = new XMLHttpRequest();
     req.open('GET', 'https://raw.githubusercontent.com/thefatpotato115/Apachi/main/src/Application.js');
     req.onload = function() {
